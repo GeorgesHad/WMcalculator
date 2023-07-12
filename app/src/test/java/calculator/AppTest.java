2fbfde65c89fc8236b42e7d58cf7fe81ff963abc
Georges <geomia2@hotmail.com>
2023-07-12 12:58:13 +0300

    made the test each in a separate function.

--- app/src/test/java/calculator/AppTest.java
+++ app/src/test/java/calculator/AppTest.java
@@ -4,11 +4,32 @@
 package calculator;
 
 import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
     @Test void appHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    }
+    @Test void add(){
+        Operation op= new Operation();
+        assertEquals(7,op.add(3,4));
+    }
+    @Test void subtract()
+    {
+        Operation op= new Operation();
+        assertEquals(1,op.subtract(5,4));
+    }
+    @Test void multiply(){
+        Operation op= new Operation();
+        assertEquals(12,op.multiply(3,4));
+    }
+    @Test void factorial()
+    {
+        Operation op= new Operation();
+        assertEquals( BigInteger.valueOf(6),op.factorial(3));
     }
 }
